a022e988140b6355232750bf3535b9013bbe248c
Zlatko <zlatko.kovachev@studenti.unipd.it>
2025-10-29 20:07:42 +0100

    - Fixed some issues with avoidance policy. It needs sone more fixes to work.

--- src/MATLAB_R2024b/avoidPolicyFunction.m
+++ src/MATLAB_R2024b/avoidPolicyFunction.m
@@ -28,9 +28,9 @@ function [avoid_policy] = avoidPolicyFunction(avoid_policy, x_pred, N, obs)
             dir = x_N_dir;
         end
 
-        % if norm(diff) <= 2
-        %     dir = x_N_dir;
-        % end
+        if norm(diff) <= 2
+            dir = x_N_dir;
+        end
 
         avoid_policy.on = true;
         avoid_policy.used = true;
@@ -42,30 +42,45 @@ function [avoid_policy] = avoidPolicyFunction(avoid_policy, x_pred, N, obs)
         avoid_policy.pos = x_N_pos + avoid_policy.margin * avoid_policy.n; 
         avoid_policy.dir = dir;
 
-            % find theta +- dev_ang
-        t1 = dir + avoid_policy.dev_ang;
-        c1 = cos(t1);
-        s1 = sin(t1); 
-        g1 = x_N_pos + [c1; s1];
-
-        t2 = dir - avoid_policy.dev_ang;
-        c2 = cos(t2);
-        s2 = sin(t2); 
-        g2 = x_N_pos + [c2; s2];
-
-        if norm(g1 - avoid_policy.goal(1:2)) < norm(g2 - avoid_policy.goal(1:2))
-            avoid_policy.single = [g1; 0];
+        if ang_diff >= 0
+            avoid_policy.theta = dir - avoid_policy.dev_ang;
+            avoid_policy.cos = cos(avoid_policy.theta);
+            avoid_policy.sin = sin(avoid_policy.theta);
+            avoid_policy.single = [x_N_pos + [avoid_policy.cos; avoid_policy.sin]; 0];
             avoid_policy.N = repmat(avoid_policy.single, [N, 1]);
-            avoid_policy.theta = t1;
-            avoid_policy.cos = c1;
-            avoid_policy.sin = s1;
         else
-            avoid_policy.single = [g2; 0];
+            avoid_policy.theta = dir + avoid_policy.dev_ang;
+            avoid_policy.cos = cos(avoid_policy.theta);
+            avoid_policy.sin = sin(avoid_policy.theta);
+            avoid_policy.single = [x_N_pos + [avoid_policy.cos; avoid_policy.sin]; 0];
             avoid_policy.N = repmat(avoid_policy.single, [N, 1]);
-            avoid_policy.theta = t2;
-            avoid_policy.cos = c2;
-            avoid_policy.sin = s2;
         end
+            
+
+        %     % find theta +- dev_ang
+        % t1 = dir + avoid_policy.dev_ang;
+        % c1 = cos(t1);
+        % s1 = sin(t1); 
+        % g1 = x_N_pos + [c1; s1];
+        % 
+        % t2 = dir - avoid_policy.dev_ang;
+        % c2 = cos(t2);
+        % s2 = sin(t2); 
+        % g2 = x_N_pos + [c2; s2];
+        % 
+        % if norm(g1 - avoid_policy.goal(1:2)) < norm(g2 - avoid_policy.goal(1:2))
+        %     avoid_policy.single = [g1; 0];
+        %     avoid_policy.N = repmat(avoid_policy.single, [N, 1]);
+        %     avoid_policy.theta = t1;
+        %     avoid_policy.cos = c1;
+        %     avoid_policy.sin = s1;
+        % else
+        %     avoid_policy.single = [g2; 0];
+        %     avoid_policy.N = repmat(avoid_policy.single, [N, 1]);
+        %     avoid_policy.theta = t2;
+        %     avoid_policy.cos = c2;
+        %     avoid_policy.sin = s2;
+        % end
 
         
 
