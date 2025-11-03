921f5120d21c56f83200185abf9576a8c6f2120a
Zlatko <zlatko.kovachev@studenti.unipd.it>
2025-11-03 19:57:16 +0100

    - Deleted files for the merge from "optimization".

--- src/MATLAB_R2024b/inputBounds.m
+++
@@ -1,7 +0,0 @@
-function [lb, ub] = inputBounds(v_max, w_max, N)
-
-    lb = repmat([0; -w_max], [N, 1]);
-    %lb = repmat([-v_max; -w_max], [N, 1]);
-    ub = repmat([v_max; w_max], [N, 1]);
-
-end
